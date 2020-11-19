8744b3172a065e5ef0e7b1315577dea50d5f20e0
SergProgMan <semibratyev@gmail.com>
2020-11-19 22:34:27 +0200

    jwt

---
+++ app/auth.go
@@ -0,0 +1,42 @@
+package app
+
+import (
+	u "lens/utils"
+	"net/http"
+	"strings"
+)
+
+var JwtAuthentication = func(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		noAuth := []string{"/api/user/new", "api/user/login"} //no auth endpoints
+		requestPath := r.URL.Path                             //current request
+
+		for _, value := range noAuth {
+			if value == requestPath {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		response := make(map[string]interface{})
+		tokenHeader := r.Header.Get("Authorization")
+
+		if tokenHeader == "" { //no token, return 403
+			response = u.Message(false, "Missing auth token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		splitted := strings.Split(tokenHeader, " ") //check token format `Bearer {token-body}`
+		if len(splitted) != 2 {
+			response = u.Message(false, "Invalid/Malformed auth token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+	})
+}
